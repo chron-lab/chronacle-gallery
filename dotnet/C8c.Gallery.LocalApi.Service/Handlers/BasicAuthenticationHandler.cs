1e03b48d8a359395c7ba67f07e521eadd8c9bd99
eskye <sunkanmiijatuyi@gmail.com>
2022-06-15 11:47:59 +0100

    Implement basic authentication on transaction controller post endpoint

---
+++ dotnet/C8c.Gallery.LocalApi.Service/Handlers/BasicAuthenticationHandler.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MultiChainDotNet.Core;
+using System;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace C8c.Gallery.LocalApi.Service.Handlers
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly IConfiguration _config;
+
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, IConfiguration config) : base(options, logger, encoder, clock)
+        {
+            _config = config;
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var endpoint = Context.GetEndpoint();
+            if (endpoint?.Metadata?.GetMetadata<IAllowAnonymous>() != null)
+                return Task.FromResult(AuthenticateResult.NoResult());
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return Task.FromResult(AuthenticateResult.Fail("Missing Authorization Header"));
+            try
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                var credentials = Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
+                var username = credentials[0];
+                var password = credentials[1];
+                var mcConfig = _config.GetSection("MultiChainConfiguration").Get<MultiChainConfiguration>();
+                if ( username != mcConfig.Node.RpcUserName || password != mcConfig.Node.RpcPassword)
+                    return Task.FromResult(AuthenticateResult.Fail("The username or password is not correct."));
+
+                var claims = new[] {   new Claim(ClaimTypes.Name, username) };
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+                return Task.FromResult(AuthenticateResult.Success(ticket));
+
+            }
+            catch
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Invalid Authorization Header"));
+            }
+        }
+    }
+}
